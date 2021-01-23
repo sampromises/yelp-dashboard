27f69a91308eac945911257a44485e4684d31cfc
Sam Kim <samkim90274@gmail.com>
2021-01-23 09:19:21 -0800

    [UI] Allow responsive for mobile

--- static/js/user.js
+++ static/js/user.js
@@ -8,11 +8,5 @@ $(document).ready(function () {
     pageLength: 50,
     order: [[REVIEW_DATE_COL, "desc"]],
     autoWidth: false,
-    columnDefs: [
-      { targets: BIZ_NAME_COL, className: "truncate-col" },
-      { targets: BIZ_ADDRESS_COL, className: "truncate-col biz-address-col" },
-      { targets: REVIEW_DATE_COL, className: "truncate-col review-date-col" },
-      { targets: STATUS_COL, className: "truncate-col status-col" },
-    ],
   });
 });
